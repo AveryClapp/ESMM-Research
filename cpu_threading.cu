03c24653b6b4a7b20e8f0122b6fbdfce72625bd6
AveryClapp <avery.clapp@gmail.com>
2024-11-04 18:10:17 +0000

    CPU threading another approach, all similar runtimes tho

--- cpu_threading.cu
+++ cpu_threading.cu
@@ -18,30 +18,26 @@
 // For every row in A (also C), create 32 threads so that each thread goes through 8 (B_COLS). This will allow for one whole row of 
 // C to be computed per thread spawn. This will eventually be lifted/modified to a CUDA implementation where you launch one warp
 // to compute a whole row of C.
-void processRows(const float* __restrict__ A,
-                  const float* __restrict__ B,
-                  float* __restrict__ C,
-                  const int startRow,
-                  const int numRows) {
-  for (int row = startRow; row < startRow + numRows; ++row) {
-    for (int j = 0; j < 8; ++j) {
-      C[row * 8 + j] = 0;
-      for (int k = 0; k < 32; ++k) {
-        C[row * 8 + j] += A[row * 32 + k] * B[k * 8 + j];
-      }
+void processColumn(const float* __restrict__ A,
+    const float* __restrict__ B,
+    float* __restrict__ C,
+    const int colA) {
+  for (int row = 0; row < 1024; row++) {
+    const float aVal = A[row * 32 + colA];
+    for (int j = 0; j < 8; j++) {
+      C[row * 8 + j] += aVal * B[colA * 8 + j];
     }
   }
 }
 
 void threadedMatrixMultiplication(const float* __restrict__ A,
-                                    const float* __restrict__ B,
-                                    float* __restrict__ C) {
+    const float* __restrict__ B,
+    float* __restrict__ C) {
+  std::memset(C, 0, 1024 * 8 * sizeof(float));
   std::vector<std::thread> threads(32);
-  const int rowsPerThread = 32;
 
-  for (int t = 0; t < 32; ++t) {
-    int startRow = t * rowsPerThread;
-    threads[t] = std::thread(processRows, A, B, C, startRow, rowsPerThread);
+  for (int t = 0; t < 32; t++) {
+    threads[t] = std::thread(processColumn, A, B, C, t);
   }
 
   for (auto& thread : threads) {

65ff6c212c51d149d11c43f48ebdb1bfc223b22d
AveryClapp <avery.clapp@gmail.com>
2025-02-27 17:22:51 +0000

    misaligned address

--- kernels/vectorized_blocktiling.cu
+++ kernels/vectorized_blocktiling.cu
@@ -11,8 +11,8 @@
 
 template <const int BM, const int BN, const int BK, const int TM, const int TN>
 __global__ void __launch_bounds__((BM * BN) / (TM * TN), 1)
-	vectorized_blocktiling(int M, int N, int K, const float *A,
-						const float *B, float *C) {
+	vectorized_blocktiling(int M, int N, int K, float *A,
+					 float *B, float *C) {
 	// Determines where the block will start
 	const uint cRow = blockIdx.y;
 	const uint cCol = blockIdx.x;
@@ -37,13 +37,10 @@ __global__ void __launch_bounds__((BM * BN) / (TM * TN), 1)
 	B += cCol * BN;
 	C += cRow * BM * N + cCol * BN;
 
-	// Get the location of the thread within A block
-	const uint innerRowA = threadIdx.x / BK;
-	const uint innerColA = threadIdx.x % BK;
-	// Define how large our "steps" are in SMEM
-
-	const uint innerRowB = threadIdx.x / BN;
-	const uint innerColB = threadIdx.x % BN;
+	const uint innerRowA = threadIdx.x / (BK / 4);
+	const uint innerColA = threadIdx.x % (BK / 4);
+  	const uint innerRowB = threadIdx.x / (BN / 4);
+	const uint innerColB = threadIdx.x % (BN / 4);
 
 	float threadResults[TM * TN] = {0.0}; // All thread results
 
@@ -62,11 +59,8 @@ __global__ void __launch_bounds__((BM * BN) / (TM * TN), 1)
 		As[(innerColA * 4 + 3) * BM + innerRowA] = tmp.w;
 
 		// Load the float4 value from global memory
-		 tmp = reinterpret_cast<const float4 *>(&B[innerRowB * N + innerColB * 4])[0];
-
-		// Store the float4 value to shared memory
-		*reinterpret_cast<float4 *>(&Bs[innerRowB * BN + innerColB * 4]) = tmp;
-		//reinterpret_cast<const float4 *>(&Bs[innerRowB * BN + innerColB * 4])[0] = reinterpret_cast< const float4 *>(&B[innerRowB * N + innerColB * 4])[0];
+		    reinterpret_cast<float4 *>(&Bs[innerRowB * BN + innerColB * 4])[0] =
+					        reinterpret_cast<float4 *>(&B[innerRowB * N + innerColB * 4])[0];
 		__syncthreads();
 
 		// Advance the matrix pointers to the start of the next block
